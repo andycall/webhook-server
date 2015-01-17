629f88f917266eb7e838807a538216d5371e1e75
andycall <dongtiangche@gmail.com>
2015-01-17 20:59:26 +0800

    finished

--- Webhock_handle.js
+++ Webhock_handle.js
@@ -1,7 +1,6 @@
-const EventEmitter = require('events').EventEmitter
-	, inherits     = require('util').inherits
-	, crypto       = require('crypto')
-	, bl           = require('bl')
+const EventEmitter = require('events').EventEmitter // 事件管理
+	, crypto       = require('crypto') // 加密模块
+	, bl           = require('bl'); // 缓存队列
 
 
 function signBlob (key, blob) {
@@ -12,59 +11,60 @@ function signBlob (key, blob) {
 // path, secret
 function create (options) {
 	if (typeof options != 'object')
-		throw new TypeError('must provide an options object')
+		throw new TypeError('must provide an options object');
 
 	if (typeof options.path != 'string')
-		throw new TypeError('must provide a \'path\' option')
+		throw new TypeError('must provide a \'path\' option');
 
 	if (typeof options.secret != 'string')
-		throw new TypeError('must provide a \'secret\' option')
+		throw new TypeError('must provide a \'secret\' option');
 
 	// make it an EventEmitter, sort of
-	handler.__proto__ = EventEmitter.prototype
-	EventEmitter.call(handler)
+	handler.__proto__ = EventEmitter.prototype;
+	EventEmitter.call(handler);
 
-	return handler
-
+	return handler;
 
 	function handler (req, res, callback) {
 		// 去除queryString
-		if (req.url.split('?').shift() !== options.path)
-			return callback()
+		if (req.url.split('?').shift() !== options.path){
+			return callback();
+		}
 
-
+		// 错误处理
 		function hasError (msg) {
-			res.writeHead(400, { 'content-type': 'application/json' })
-			res.end(JSON.stringify({ error: msg }))
+			res.writeHead(400, { 'content-type': 'application/json' });
+			res.end(JSON.stringify({ error: msg }));
 
-			var err = new Error(msg)
+			var err = new Error(msg);
 
-			handler.emit('error', err, req)
+			handler.emit('error', err, req);
 			callback(err)
 		}
 
 		var sig   = req.headers['x-hub-signature']
 			, event = req.headers['x-github-event']
-			, id    = req.headers['x-github-delivery']
+			, id    = req.headers['x-github-delivery'];
 
 		if (!sig)
-			return hasError('No X-Hub-Signature found on request')
+			return hasError('No X-Hub-Signature found on request');
 
 		if (!event)
-			return hasError('No X-Github-Event found on request')
+			return hasError('No X-Github-Event found on request');
 
 		if (!id)
-			return hasError('No X-Github-Delivery found on request')
+			return hasError('No X-Github-Delivery found on request');
 
+		// 通过缓存队列保存流, data是最后的输出流
 		req.pipe(bl(function (err, data) {
 			if (err) {
 				return hasError(err.message)
 			}
 
-			var obj
+			var obj;
 
 			if (sig !== signBlob(options.secret, data))
-				return hasError('X-Hub-Signature does not match blob signature')
+				return hasError('X-Hub-Signature does not match blob signature');
 
 			try {
 				obj = JSON.parse(data.toString())
@@ -72,7 +72,7 @@ function create (options) {
 				return hasError(e)
 			}
 
-			res.writeHead(200, { 'content-type': 'application/json' })
+			res.writeHead(200, { 'content-type': 'application/json' });
 			res.end('{"ok":true}');
 
 			handler.emit(event, {
--- test.js
+++ test.js
@@ -1,35 +1,61 @@
 var http = require('http');
 var createHandler = require('./Webhock_handle');
 var handler = createHandler({ path : "/webhook" , 'secret' : "c7e41fd7aa6903fc0d2cd560a49b43084d23e32f"});
+var exec = require("child_process").exec;
+var fs = require("fs");
+var stdout = process.stdout;
+var stdin = process.stdin;
+var path = require('path');
+var test =  require('tape');
+var dirlog = "./log/";
 
-http.createServer(function(req, res){
-	handler(req, res, function(err){
-		res.statusCode = 404;
-		res.end("ERROR");
+process.on('uncaughtException', function(err){
+	setTimeout(startServer, 5000);
+});
+
+function getTime(){
+	var date = new date();
+	return date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
+}
+
+function log(msg){
+	var time = getTime();
+	fs.appendFileSync(dirlog + time + ".log", msg);
+}
+
+function createLogDir(){
+	if(!fs.existsSync(dirlog) || fs.statSync(dirlog).isDirectory()) {
+		fs.mkdirSync(dirlog);
+	}
+}
+
+function startServer(){
+	http.createServer(function(req, res){
+		handler(req, res, function(err){
+			res.statusCode = 404;
+			res.end("ERROR");
+		});
+		createLogDir();
+	}).listen(7890);
+
+	handler.on('error', function(msg){
+		console.log("ERROR: ", msg);
 	});
-}).listen(7890);
 
+	handler.on('push', function(event){
+		console.log('receive an push event for %s to %s', event.payload.repository.name, event.payload.ref);
+		log("---------- " + getTime() + ' ' + event.payload.repository.name + ' ------------\n');
+		
+	});
 
-
-handler.on('error', function(msg){
-	console.log("ERROR: ", msg);
-});
-
-handler.on('push', function(event){
-	console.log('receive an push event for %s to %s', event.payload.repository.name, event.payload.ref);
-});
-
-handler.on('issue', function(event){
-	console.log('Received an issue event for % action=%s: #%d %s',
-		event.payload.repository.name,
-		event.payload.action,
-		event.payload.issue.number,
-		event.payload.issue.title)
-});
-
-
-
-
-
+	handler.on('issue', function(event){
+		console.log('Received an issue event for % action=%s: #%d %s',
+			event.payload.repository.name,
+			event.payload.action,
+			event.payload.issue.number,
+			event.payload.issue.title);
+	});
+}
+startServer();
 
 
